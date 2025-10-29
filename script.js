25706b83298035284744a125cbecfa79c78713de
VILOJETHAN <99376471+VILOJETHAN@users.noreply.github.com>
2025-10-29 20:47:45 +0530

    Design and responsiveness check

---
+++ script.js
@@ -0,0 +1,11 @@
+let student={
+
+    firstname: "Vilojethan",
+    lastname: "Vijayakumar",
+    email: "sample@gmail.com",
+    department: "BE",
+}
+
+console.log(student.firstname);
+
+// create a  student object which have the firstname, lastname, email,phone and department like bsc,msc and mca
